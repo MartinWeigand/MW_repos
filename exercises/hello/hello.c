048cd4f686629b48c12e258da7332d539accb6d0
MartinWeigand <mweigand97@gmail.com>
2021-02-07 11:49:22 +0100

    changed output message

--- exercises/hello/hello.c
+++ exercises/hello/hello.c
@@ -2,6 +2,6 @@
 #include "math.h"  // for html display only, you should use angular brackets
 int main(void){
 	double r=sqrt(2.0);
-	printf("hello, sqrt(2)=%g\n",r);
+	printf("hello, world. Did you know that sqrt(2)=%g?\n",r);
 return 0;
 }
