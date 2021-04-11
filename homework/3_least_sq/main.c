2d7714fd0365da5317256a77bae5ed85c040d1ad
MartinWeigand <mweigand97@gmail.com>
2021-04-11 15:51:20 +0200

    All tasks are done

--- homework/3_least_sq/main.c
+++ homework/3_least_sq/main.c
@@ -4,7 +4,6 @@
 #include<gsl/gsl_vector.h>
 #include<gsl/gsl_matrix.h>
 #include"functions.h"
-#define FMT "%7.3f"
 
 int main(){
 	double x[]  = {1,  2,  3, 4, 6, 9,   10,  13,  15};
@@ -18,7 +17,6 @@ int main(){
 		dy[i] /= y[i];
 		y[i] = log(y[i]);
 	}
-//	printf("%10g %10g %10g %10g %10g %10g",dy[0],dy[1],dy[2],dy[3],dy[4],dy[5]);
 	gsl_vector* vx = gsl_vector_alloc(n);
 	gsl_vector* vy = gsl_vector_alloc(n);
 	gsl_vector* vdy = gsl_vector_alloc(n);
@@ -40,43 +38,50 @@ int main(){
 	gsl_vector* c = gsl_vector_alloc(m);
 	gsl_matrix* S = gsl_matrix_alloc(m,m);
 	lsfit(m,funs,vx,vy,vdy,c,S);
-//	gsl_vector_fprintf(stdout,c,FMT);
-//	gsl_vector* dc = gsl_vector_alloc(m);
-//	for(int k=0;k<m;k++){
-//		double skk=gsl_matrix_get(S,k,k);
-//		gsl_vector_set(dc,k,sqrt(skk));
-//		}
-//
-//	double fit(double x){
-//		double s=0;
-//		for(int k=0;k<m;k++)s+=gsl_vector_get(c,k)*funs(k,x);
-//		return s;
-//		}
-//
-//	double fit_plus(int i, double x){
-//		return fit(x)+gsl_vector_get(dc,i)*funs(i,x);
-//		}
-//
-//	double fit_minus(int i, double x){
-//		return fit(x)-gsl_vector_get(dc,i)*funs(i,x);
-//		}
-//
-//	double c1 =gsl_vector_get(c, 1);
-//	double dc1=gsl_vector_get(dc,1);
-//	double T=-1/c1*log(2.0);
-//	double dT=dc1/c1/c1;
-//	printf("# half-life = %.3g +- %.2g days\n",T,dT);
-//
-//	printf("# time log(activity) delta(log(activity))\n");
-//	for(int i=0;i<n;i++)printf("%g %g %g\n",x[i],y[i],dy[i]);
-//	printf("\n\n");
-//
-//	for(int i=0;i<m;i++){
-//		printf("# time fit fit_plus fit_minus; k=%i\n",i);
-//		for(double z=x[0],dz=(x[n-1]-x[0])/64;z<=x[n-1];z+=dz)
-//		printf("%g %g %g %g\n",z,fit(z),fit_plus(i,z),fit_minus(i,z));
-//	printf("\n\n");
-//	}
-//
+
+	gsl_vector* delta_c = gsl_vector_alloc(m);
+	for(int k=0; k<m; k++){
+		double S_kk = gsl_matrix_get(S,k,k);
+		gsl_vector_set(delta_c,k,sqrt(S_kk));
+	}
+	double c1 = gsl_vector_get(c,1);
+	double delta_c1 = gsl_vector_get(delta_c,1);
+	double half_life = -log(2)/c1;
+	double delta_half_life = delta_c1/c1/c1; //from propogation of errors
+	printf("# The fit gives ln(y)=%g(+-%g)%g(+-%g)*t. This gives a half-life of ln(2)/%g=%g+-%g days. According to wikipedia the modern value is 3,6319 days. Hence the half-life doesn't agree with the moderne value within the estimated uncertainty.\n",gsl_vector_get(c,0),gsl_vector_get(delta_c,0),c1,delta_c1,-c1,half_life,delta_half_life);
+	printf("\n");
+	printf("# time ln(activity) uncertainty\n");
+	for(int i=0; i<n; i++){
+		printf("%10g %10g %10g\n",x[i],y[i],dy[i]);
+	}
+	printf("\n\n");
+	double fit(double x){
+		double s=0;
+		for(int k=0; k<m; k++){
+			s+=gsl_vector_get(c,k)*funs(k,x);
+		}
+		return s;
+	}
+	printf("# time fit\n");
+	for(double z=x[0],dz=(x[n-1]-x[0])/64;z<=x[n-1];z+=dz){
+		printf("%g %g\n",z,fit(z));
+	}
+
+	double fit_plus(int i, double x){
+		return fit(x)+gsl_vector_get(delta_c,i)*funs(i,x);
+	}
+
+	double fit_minus(int i, double x){
+		return fit(x)-gsl_vector_get(delta_c,i)*funs(i,x);
+	}
+	printf("\n\n");
+	for(int i=0;i<m;i++){
+		printf("# time fit_plus fit_minus; k=%i\n",i);
+		for(double z=x[0], dz=(x[n-1]-x[0])/64; z<=x[n-1]; z+=dz){
+			printf("%g %g %g\n",z,fit_plus(i,z),fit_minus(i,z));
+		}
+	printf("\n\n");
+	}
+
 return 0;
 }
